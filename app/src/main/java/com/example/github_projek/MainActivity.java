30f6f706c7947c826b51b7a9b55bbaffee87186d
DominicusAgfid <agfid6661@gmail.com>
2021-06-20 10:09:58 +0700

    API GITHUB

---
+++ app/src/main/java/com/example/github_projek/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.github_projek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.github_projek.database.AkunDao;
+import com.example.github_projek.database.AkunDatabase;
+import com.example.github_projek.model.db.Akun;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText user, pass;
+    Button login, register;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        SharedPreferences getPreferences = getSharedPreferences("SAVED_LOGIN", MODE_PRIVATE);
+        Boolean check = getPreferences.getBoolean("LOGGED",false);
+        String username = getPreferences.getString("USERNAME", "DEFAULT");
+
+        if (check){
+            Intent intent = new Intent(MainActivity.this , NavbarActivity.class);
+            MainActivity.this.startActivity(intent);
+            finish();
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        user = findViewById(R.id.username);
+        pass = findViewById(R.id.password);
+        login = findViewById(R.id.btn_login);
+        register = findViewById(R.id.btn_register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MainActivity.this,DaftarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = user.getText().toString();
+                String password = pass.getText().toString();
+
+
+                // Check if username, password is filled
+
+                if(username.trim().length() == 0 || password.trim().length() == 0 ){
+                    //JIka Username Kosong
+                    Toast.makeText(getApplicationContext(),"Tidak Berhasil Login", Toast.LENGTH_LONG).show();
+                }else{
+                    AkunDatabase akunDatabase = AkunDatabase.getInstance(getApplicationContext());
+                    AkunDao akunDao = akunDatabase.akunDao();
+
+                    Akun akun = akunDao.login(username,password);
+                    if (akun == null){
+                        Toast.makeText(getApplicationContext(), "Akun Tidak Ada", Toast.LENGTH_SHORT).show();
+                    }else{
+                        SharedPreferences sharedPreferences = getSharedPreferences("SAVED_LOGIN", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putBoolean("LOGGED",true);
+                        editor.putString("USERNAME",username);
+                        editor.apply();
+
+                        Intent intent = new Intent(getApplicationContext(), NavbarActivity.class);
+                        Toast.makeText(getApplicationContext(),"Selamat Datang " + username, Toast.LENGTH_LONG).show();
+                        MainActivity.this.startActivity(intent);
+                        finish();
+                    }
+                }
+
+            }
+        });
+
+    }
+}
